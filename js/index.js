93d8f62e6dc67e9c1e06bbbdcffd39e6ae7f759b
oiiemon9 <oiiemon9@gmail.com>
2025-09-04 10:11:20 +0600

    lesson button add

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log('object');
+const Lessons = () => {
+  fetch('https://openapi.programming-hero.com/api/levels/all')
+    .then((res) => res.json())
+    .then((button) => lesssonButtons(button.data));
+};
+
+const lesssonButtons = (data) => {
+  const buttonSection = document.getElementById('lesson-btn-section');
+  data.map((button) => {
+    console.log(data);
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+      <button class="btn btn-outline btn-primary">
+        <i class="fa-solid fa-book-open"></i>Lesson - ${button.level_no}
+      </button>
+      `;
+    buttonSection.appendChild(newDiv);
+  });
+};
+
+Lessons();
